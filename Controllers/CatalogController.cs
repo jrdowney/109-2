525200736d6eb58f1e76c089a27b7d61057ff2c7
jrdowney <64438369+jrdowney@users.noreply.github.com>
2020-07-30 22:53:26 -0500

    Add files via upload

---
+++ Controllers/CatalogController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PropertyRental.Controllers {
+
+    public class CatalogController : Controller {
+    
+        public IActionResult Test(){
+            return View();
+        }
+
+        public IActionResult Index(){
+            return View();
+        }
+
+        public IActionResult Admin(){
+            return View();
+        }
+    }
+}
